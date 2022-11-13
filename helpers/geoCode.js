8f8a8d3ad209d5282e5dcb7f16dea13bceb44ae7
daniel kazarian <dokazarian@gmail.com>
2022-11-12 17:41:26 -0800

    Main + more helpers
    - Added blocks, lots helpers
    - Added main.js
    - Added .env file
    - Moved token API to helpers
    - Geocode helper now returns an object of latitude + longitude key-value pairs

--- helpers/geoCode.js
+++ helpers/geoCode.js
@@ -1,13 +1,21 @@
 const fetch = require('node-fetch');
+require("dotenv").config();
 
-const mapboxAccessToken = 'pk.eyJ1IjoiZGFuaWVsa2F6YXJpYW4iLCJhIjoiY2xhZWx1b2phMGF3NTNxbzVtZ3kybXA0aCJ9.mgVtnNOz8WaEgGlqxSs7Mw';
 
 function geoCode(location) {
     let encodedLocation = location.replace(' ', '%20');
 
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedLocation}.json?access_token=${mapboxAccessToken}`)
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedLocation}.json?access_token=${process.env.mapboxAccessToken}`)
     .then((response) => response.json())
-    .then(data => console.log(data['features'][0]['geometry']['coordinates']));
+    .then(function(data) {
+        [longitude, latitude] = data['features'][0]['geometry']['coordinates'];
+
+        return {
+            longitude: longitude,
+            latitude: latitude
+        }
+
+    });
 }
 
 module.exports = { geoCode };
